8fb08074253cdbbe0a7599e459f14a9130aff53a
WorkHard7 <solinuxnet@gmail.com>
2022-08-02 15:32:45 +0300

    checking just one hole at a time

--- app.js
+++ app.js
@@ -3,17 +3,15 @@ console.log("Initial rabbit position: ", rabbit);
 
 function search(rabbit) {
   let i;
-  let prev;
 
-  for (i = 1, prev = i - 1; i < 101; i++, prev++) {
+  for (i = 1; i < 101; i++) {
     let changePosRandom = Math.floor(Math.random() * 10);
 
     console.log("current is", i);
-    console.log("prev is", prev);
     console.log("rabbit at", rabbit);
     console.log("changePosRandom", changePosRandom);
 
-    if (i == rabbit || prev == rabbit) {
+    if (i == rabbit) {
       return i;
     } else {
       if (rabbit < 100) {
@@ -22,6 +20,16 @@ function search(rabbit) {
         } else {
           rabbit--;
         }
+      } else {
+        continue;
+      }
+
+      console.log("current is", i);
+      console.log("rabbit at", rabbit);
+      console.log("changePosRandom", changePosRandom);
+
+      if (i == rabbit) {
+        return i;
       } else {
         continue;
       }
@@ -35,4 +43,4 @@ function search(rabbit) {
 let result = search(rabbit);
 result == -1
   ? document.write("Rabbit escaped! ")
-  : document.write("Rabbit was hidding in the hole number: ", result);
+  : document.write(`Rabbit was hidding in the hole number: ${result}`);
